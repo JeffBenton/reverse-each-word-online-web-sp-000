ab532b22aae99378703da48838c8aef5bc34d179
Jeff Benton <sikletz@gmail.com>
2019-01-06 06:08:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
-  arr = []
-  str.split.each do |x|
-    arr << x.reverse
-  end
-  arr.join(" ")
+  # arr = []
+  # str.split.each do |x|
+  #   arr << x.reverse
+  # end
+  # arr.join(" ")
 end
